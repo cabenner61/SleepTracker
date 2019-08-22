30e2ed0ffd39ab6c27b9a723286fa7f371124b38
CAB <cabenner61@outlook.com>
2019-08-22 16:25:37 -0700

    Version 0.3 WellRestedIntentHandler Added random variety to speech output

--- index.js
+++ index.js
@@ -1,10 +1,43 @@
 // This sample demonstrates handling intents from an Alexa skill using the Alexa Skills Kit SDK (v2).
-// Please visit https://alexa.design/cookbook for additional examples on implementing slots, dialog management,
-// session persistence, api calls, and more.
 const Alexa = require('ask-sdk-core');
-//
+// BASELINE
 // used the sample to build Sleep Tracker's WellRestedIntent()
+// Note: also removed HelloWorld intent handler from sample
+//       changed responses in HelpIntentHandler and LaunchRequestHandler accordingly
 //
+// BEGIN Version 0.3
+// add some variety to speech responses
+// implement pluck() and WellRestedPhrases
+//
+function pluck (arr) {
+  const randIndex = Math.floor(Math.random() * arr.length);
+  return arr[randIndex];
+}
+
+const WellRestedPhrases = {
+  tooMuch: [
+    "I think you may sleep too much and swing back to tired.",
+    "whoa, that's a lot of sleep. You'll wake up rested for sure."
+  ],
+  justRight: [
+    "you should wake up refreshed.",
+    "it's clear you know rest is important. Good job.",
+    "it's clear you know rest is important. Good job!",
+    "with that much sleep, you're ready to face the world.",
+    "you'll wake up invigorated."
+  ],
+  justUnder: [
+    "you may get by, but watch out for a mid-day crash.",
+    "you'll be alright, but would be better off with a bit more time.",
+    "you might be a little tired tomorrow."
+  ],
+  tooLittle: [
+    "you'll be dragging tomorrow. Get the coffee ready!",
+    "that's either a long night or early morning? Either way, tomorrow's going to be rough."
+  ]
+};
+// END Version 0.3;
+
 const WellRestedIntentHandler = {
   canHandle(handlerInput) {
     const intentName = "WellRestedIntent";
@@ -42,13 +75,13 @@ const WellRestedIntentHandler = {
       }
 
       if(adjustedHours > 12) {
-        speech = "I think you may sleep too much and swing back to tired.";
+            speech += pluck(WellRestedPhrases.tooMuch);
       } else if(adjustedHours > 8) {
-        speech = "You should wake up refreshed.";
+            speech += pluck(WellRestedPhrases.justRight);
       } else if(adjustedHours > 6) {
-        speech = "You may get by, but watch out for a mid-day crash.";
+            speech += pluck(WellRestedPhrases.justUnder);
       } else {
-        speech = "You'll be dragging. Get the coffee ready!";
+            speech += pluck(WellRestedPhrases.tooLittle);
       }
 
       return handlerInput
@@ -76,7 +109,7 @@ const LaunchRequestHandler = {
         return Alexa.getRequestType(handlerInput.requestEnvelope) === 'LaunchRequest';
     },
     handle(handlerInput) {
-        const speakOutput = 'Welcome, you can say Hello or Help. Which would you like to try?';
+        const speakOutput = 'Welcome, you can ask about sleeping or say Help. Which would you like to try?';
         return handlerInput.responseBuilder
             .speak(speakOutput)
             .reprompt(speakOutput)
@@ -90,7 +123,7 @@ const HelpIntentHandler = {
             && Alexa.getIntentName(handlerInput.requestEnvelope) === 'AMAZON.HelpIntent';
     },
     handle(handlerInput) {
-        const speakOutput = 'You can say hello to me! How can I help?';
+        const speakOutput = 'You can ask, for example, How rested will I be if I get 6 hours of sleep';
 
         return handlerInput.responseBuilder
             .speak(speakOutput)
